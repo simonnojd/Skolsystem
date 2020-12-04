a65412454f3dd5b1f2ba91bb78f00ae83762248c
yadonx <ej_yo@hotmail.com>
2020-12-04 16:05:42 +0100

    vårstädning

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Database d = new Database();
-        System.out.println(d.searchCourse("Biologi"));
 
         UserLoggedIn u = new UserLoggedIn();
 
