6e2074fd46344f3045ffd788dbce2500a0fa390d
Liliana-MP <liliana.pitra@gmail.com>
2020-12-04 15:53:52 +0100

    Added some more GUI

--- src/UserLoggedIn.java
+++ src/UserLoggedIn.java
@@ -10,22 +10,49 @@ import java.awt.*;
  */
 public class UserLoggedIn extends JFrame{
 
-    private JLabel schoolInfo;
-    private JTextArea searchField;
-    private JButton courseSearch;
-    private JButton teacherSearch;
-    private JButton studentSearch;
-    private JButton clearTextArea;
-    private JTextField textField;
-    private JPanel bottomPanel;
-    private JPanel northPanel;
+    private JLabel schoolInfo = new JLabel("Välkommen till Nackademin!");
+    private JTextArea infoField = new JTextArea("Info......");
+    private JButton courseSearch = new JButton("Sök efter kurser");
+    private JButton teacherSearch = new JButton("Sök efter lärare");
+    private JButton studentSearch = new JButton("Sök efter elever");
+    private JButton clearTextArea = new JButton("Rensa sökfält");
+    private JTextField searchField = new JTextField("Skriv här");
+    private JPanel bottomPanel = new JPanel();
+    private JPanel northPanel = new JPanel();
+    private JPanel southPanel = new JPanel();
+    private JPanel centerPanel = new JPanel();
+    // private JPanel westPanel =  new JPanel();
 
 
     public UserLoggedIn(){
         bottomPanel.setLayout(new BorderLayout());
         northPanel.setLayout(new BorderLayout());
+        southPanel.setLayout(new GridLayout(2, 2));
+        centerPanel.setLayout(new BorderLayout());
 
 
+        bottomPanel.add(northPanel, BorderLayout.NORTH);
+        bottomPanel.add(southPanel, BorderLayout.SOUTH);
+        bottomPanel.add(centerPanel, BorderLayout.CENTER);
+
+        northPanel.add(schoolInfo, BorderLayout.NORTH);
+
+        southPanel.add(teacherSearch);
+        southPanel.add(studentSearch);
+        southPanel.add(courseSearch);
+        southPanel.add(clearTextArea);
+
+        centerPanel.add(infoField, BorderLayout.NORTH);
+        centerPanel.add(searchField, BorderLayout.SOUTH);
+
+        infoField.setEditable(false);
+
+
+        add(bottomPanel);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
 
